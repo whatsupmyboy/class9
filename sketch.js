e6e84bf99ba3b45a4499f4c53cb9cb35f306f035
whatsupmyboy <96091857+whatsupmyboy@users.noreply.github.com>
2021-12-13 20:01:34 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+
+function setup() {
+  createCanvas(400,400);
+ var wall1 = createSprite (200,200,100,100);
+wall1.shapeColor="blue"
+
+}
+
+
+function draw() 
+{
+  background(0);
+drawSprites()
+}
+
+
+
+
